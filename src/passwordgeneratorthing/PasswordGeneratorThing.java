e9c3b12b0852a4f636d50b532e686ddf6718f4d0
Chris Samuelson <chris.sam55@gmail.com>
2014-07-29 16:27:01 -0400

    basic gui setup

--- src/passwordgeneratorthing/PasswordGeneratorThing.java
+++ src/passwordgeneratorthing/PasswordGeneratorThing.java
@@ -1,17 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package passwordgeneratorthing;
 
-/**
- *
- * @author Chris
- */
 public class PasswordGeneratorThing{
-  /**
-   * @param args the command line arguments
-   */
   public static void main(String[] args){
     // TODO code application logic here
   }
